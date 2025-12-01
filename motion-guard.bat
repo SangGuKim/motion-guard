736f9c953a3a52094fa6c7e71cfa13894697526e
cymon <cymon@litbig.com>
2025-12-01 14:02:14 +0900

    feat(condae): 확장된 Conda 환경 활성화 기능 추가
    - 'motion-guard' 환경 초기화 및 활성화 스크립트 검색 기능 도입
    - 다중 위치에서 활성화 스크립트 탐색 시도

--- motion-guard.bat
+++ motion-guard.bat
@@ -1,2 +1,17 @@
 @echo off
+set ENV_NAME=motion-guard
+where conda >nul 2>nul
+if %errorlevel% equ 0 (
+    echo ### Attempting Conda environment activation: %ENV_NAME%
+    call conda activate %ENV_NAME%
+    goto execute
+)
+for %%D in ("%USERPROFILE%\anaconda3" "%USERPROFILE%\miniconda3" "C:\ProgramData\anaconda3" "C:\ProgramData\miniconda3") do (
+    if exist "%%D\Scripts\activate.bat" (
+        echo ### Conda not initialized. Searching for activate.bat to activate %ENV_NAME%.
+	call %%D\Scripts\activate.bat %ENV_NAME%
+	goto execute
+    )
+)
+:execute
 python "%~dp0\motion-guard.py" %*
